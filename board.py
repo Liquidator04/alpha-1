70c4c79129ee71b0134d2908e1b6fd2c875a895b
Adarsh Mohapatra <114862727+wanderer04@users.noreply.github.com>
2023-08-28 23:52:14 +0530

    Changes in creating the board array
    For black it should be king, queen and for white it should be queen, king.
    Also instead of appending li in the for loop, I made it append individual new lists. This eliminates a lot of bugs due to referencing.

--- board.py
+++ board.py
@@ -5,25 +5,24 @@ class board:
             self.board.append(['BR','BN','BB','BQ','BK','BB','BN','BR'])
             li=['BP' for _ in range(0,8)]
             self.board.append(li)
-            li=[0 for _ in range(0,8)]
-            for _ in range(3,7):
-                self.board.append(li)
+            for _ in range(2,6):
+                self.board.append([0 for _ in range(0,8)])
             li=[]
             li=['WP' for _ in range(0,8)]
             self.board.append(li)
             self.board.append(['WR','WN','WB','WQ','WK','WB','WN','WR'])
         else:
             li=['WP' for _ in range(0,8)]
-            self.board.append(['WR','WN','WB','WQ','WK','WB','WN','WR'])
+            self.board.append(['WR','WN','WB','WK','WQ','WB','WN','WR'])
             self.board.append(li)
-            li=[0 for _ in range(0,8)]
-            for _ in range(3,7):
-                self.board.append(li)
+            for _ in range(2,6):
+                self.board.append([0 for _ in range(0,8)])
             li=[]
             li=['BP' for _ in range(0,8)]
             self.board.append(li)
-            self.board.append(['BR','BN','BB','BQ','BK','BB','BN','BR'])
+            self.board.append(['BR','BN','BB','BK','BQ','BB','BN','BR'])
 
 
 b=board("BLACK")
-print(b.board)
+for row in b.board:
+    print(row)
